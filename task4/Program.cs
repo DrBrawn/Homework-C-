61f9106e5cb596547e9c0c62c01ca0570f91625f
DrBrawn <drbrawn@yandex.ru>
2023-02-19 02:43:22 +0300

    Complete task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+int count = 1;
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        System.Console.Write(count);
+    }
+    count += 1;
+}
